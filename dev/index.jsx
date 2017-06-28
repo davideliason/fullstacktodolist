5fad8cd6f90397ebad85574759f3fc281c6560c8
davideliason <davethemaker@gmail.com>
2017-06-28 16:05:03 -0700

    update styling for the parent ToDoList component with simple titles

--- dev/index.jsx
+++ dev/index.jsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 
 class ToDoList extends React.Component {
 	render(){
+		const toDoListStyle = {
+			backgroundColor: "yellow",
+			width: 250,
+			border: "1px solid black",
+			marginLeft: 200,
+			marginTop: 50,
+			textAlign: "center"
+		};
 
 		return(
-				<div>
-					<p>hi</p>
+				<div style={toDoListStyle}>
+				  <div>
+					<h3>ToDoList</h3>
+					<h4>fullstack!</h4>
+				  </div>
 				</div>
 			);
 	}
